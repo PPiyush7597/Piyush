7ac44bf44768f0ef69e735cfe0abb5b2e32a55b5
Piyush Khandelwal <120543064+PPiyush7597@users.noreply.github.com>
2023-02-17 21:10:12 +0530

    Create Print_5_intiger.java

---
+++ Print_5_intiger.java
@@ -0,0 +1,9 @@
+public class Print_5_intiger
+ {  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115,n3=45,n4=34,n5=70, sum;  
+sum = n1 + n2 + n3 + n4 + n5;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+} 
